3266dc4b18fce6f9cd4b5aefbebb3aa342010de6
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-08 10:54:58 -0700

    added some images to the gallery and styled the cards with grayscale

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,17 +8,12 @@
 <header>
 <nav class="bg-orange-950">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
       <Icon icon="fluent-mdl2:my-movies-t-v" color="red" width="90" height="90" />
         
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Marvel</span>
     </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
+    
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="md:flex items-center gap-x-8">
         <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><li>
--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,9 +1,37 @@
 <figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
   <a href="#">
+    <img class="rounded-lg" src="images/avengers-1.jpg" alt="image description">
+  </a>
+  <figcaption class="absolute px-4 text-lg text-white bottom-6">
+  </figcaption>
+</figure>
+<h1 class="font-[WorkSans] text-3xl">Phase 1</h1>
+<p class="font-[WorkSans]">Where it all began</p>
+
+<figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+  <a href="#">
+    <img class="rounded-lg" src="images/guardians-of-the-galaxy.jpg" alt="image description">
+  </a>
+  <figcaption class="absolute px-4 text-lg text-white bottom-6">
+      <p>Phase 2</p>
+  </figcaption>
+</figure>
+
+<figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+  <a href="#">
+    <img class="rounded-lg" src="images/spider-man-homecoming.jpg" alt="image description">
+  </a>
+  <figcaption class="absolute px-4 text-lg text-white bottom-6">
+      <p>Phase 3</p>
+  </figcaption>
+</figure>
+
+<figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 m-8 my-10 grid md:grid-cols-2 gap-6 space-x-2">
+  <a href="#">
     <img class="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description">
   </a>
   <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Movie Card</p>
+      <p>Phase 4</p>
   </figcaption>
 </figure>
 
@@ -12,15 +40,9 @@
     <img class="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description">
   </a>
   <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Movie Card</p>
+      <p>Phase 5</p>
   </figcaption>
 </figure>
 
-<figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-  <a href="#">
-    <img class="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description">
-  </a>
-  <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Movie Card</p>
-  </figcaption>
-</figure>
+
+
