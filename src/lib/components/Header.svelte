6c2723fa574da0b2122fde0ff3a7b4259140e347
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-08 09:11:16 -0700

    Images added for cards

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,12 +5,13 @@
 <!-- Flowbite-->
 
 
-
+<header>
 <nav class="bg-orange-950">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <Icon icon="fluent-mdl2:my-movies-t-v" color="red" width="90" height="90" />
+        
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Marvel</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -19,17 +20,19 @@
         </svg>
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="/movies" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Movies</a>
-        </li>
-        <li>
-          <a href="/about" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
+      <ul class="md:flex items-center gap-x-8">
+        <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><li>
+          <a class="font-semibold text-black" href="/movies"><Icon icon="fluent-mdl2:my-movies-t-v" color="black" width="90" height="50" />Movies</a>
+        </li></button>
+        <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> <li>
+          <a class="font-semibold text-black" href="/about"><Icon icon="fluent-mdl2:my-movies-t-v" color="black" width="90" height="50" />About</a>
+        </li></button>
       </ul>
     </div>
   </div>
 </nav>
+</header>
+
 
 
 <!-- <header class="md: static bg-orange-950 flex justify-between p-2">
@@ -106,3 +109,4 @@
     </ul>
   </nav>
 </header> -->
+
