a5de26ad83439e19f328a5e7a398014bb51af55f
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-08 23:05:38 -0700

    about page including tv shows image of marvel

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,14 @@
-<img class="rounded-lg w-98 h-76" src="images/allphases-collection.jpg" alt="">
-<img class="bg-cover rounded-lg w-96 h-76" src="images/phase-5-img.jpg">
-
-
 <body class="bg-orange-950 text-3xl text-white font- [WorkSans]">
+
+<img class="rounded-lg w-98 h-76" src="images/allphases-collection.jpg" alt="grid-style image of marvel related movies that span from each phase">
+<img class="rounded-lg w-98 h-76" src="images/phase-5-img.jpg" alt="image of marvel movie collection">
+<img class="rounded-lg w-98 h-76" src="images/marvel-tvshows.jpg" alt="">
+
+
   Welcome to a snippet of my Marvel collection which features all Marvel based movies that spanned from 2008 to present-day. 
   All my years of Marvel movie watching has added up to my inspiration to create this grouped out assorted cards that feature how each Marvel movie are sized down into specific 'phases' that tie in to the Marvel Cinematic Universe.
   Hope you enjoy!
-  <div>
-  </div>
+  
 </body>
 
   
--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,22 +1,25 @@
 <div class="bg-orange-950">
-  <div>
-  <iframe class="w-full aspect-video " src="https://www.youtube.com/watch?v=sXT4uBpGxNY"></iframe>
+  <div class="mx-auto">
+  
+    <iframe class="w-98 aspect-video" width="560" height="315" src="https://www.youtube.com/embed/sXT4uBpGxNY?si=D5vRlDmjMS3qkpUK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  
  </div>
 
  <div>
-  <iframe class="w-full aspect-video " src="https://www.youtube.com/watch?v=d96cjJhvlMA"></iframe>
+  <iframe class="w-98 aspect-video" width="560" height="315" src="https://www.youtube.com/embed/d96cjJhvlMA?si=H98j1Lj0N2_-NEYa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
  </div>
 
  <div>
-  <iframe class="w-full aspect-video " src="https://www.youtube.com/watch?v=rk-dF1lIbIg"></iframe>
+  <iframe class="w-98 aspect-video" width="560" height="315" src="https://www.youtube.com/embed/rk-dF1lIbIg?si=rkoSp0CKHS4dkqRI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  
  </div>
 
  <div>
-  <iframe class="w-full aspect-video " src="https://www.youtube.com/watch?v=RxAtuMu_ph4"></iframe>
+  <iframe class="w-98 aspect-video" width="560" height="315" src="https://www.youtube.com/embed/RxAtuMu_ph4?si=4_wxqx8UX1Dd3WHy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
  </div>
 
  <div>
-  <iframe class="w-full aspect-video " src="https://www.youtube.com/watch?v=wS_qbDztgVY"></iframe>
+  <iframe class="w-98 aspect-video" width="560" height="315" src="https://www.youtube.com/embed/wS_qbDztgVY?si=apKOynh3kML_LrkI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
  </div>
 
  
