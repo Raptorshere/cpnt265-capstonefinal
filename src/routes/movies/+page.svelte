1d4bec291f6805e2ae560f2cefcfad9ce466ad90
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-08 22:20:57 -0700

    footer svg size bigger; card descriptions

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,88 +1,46 @@
 <body class="bg-orange-950">
   
-  <div class="lg:inline-grid grid-cols-3 gap-4">
+  <div class="lg:inline-grid grid-cols-2 gap-4 m-auto mx-44">
   <figure class="h-auto max-w-md rounded-lg shadow-xl dark:shadow-gray-800 relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
     <a href="/list">
       <img class="rounded-lg" src="images/avengers-1.jpg" alt="image description">
     </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Phase 1</p>
-    </figcaption>
   </figure>
-  <h1 class="font-[WorkSans] text-3xl text-white">Phase 1</h1>
   <p class="font-[WorkSans] text-white">Where it all began</p>
+  <h1 class="font-[WorkSans] text-3xl text-white">Phase 1</h1>
   
   <figure class="h-auto max-w-xl ms-auto rounded-lg shadow-xl dark:shadow-gray-800 relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
     <a href="/list">
       <img class="rounded-lg" src="images/guardians-of-the-galaxy.jpg" alt="image description">
     </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Phase 2</p>
-    </figcaption>
   </figure>
+  <p class="font-[WorkSans] text-white">Tipping the cosmic scales to balance</p>
   <h1 class="font-[WorkSans] text-3xl text-white">Phase 2</h1>
-  <p class="font-[WorkSans] text-white">The story that started them off</p>
   
   
   <figure class="h-auto max-w-md rounded-lg shadow-xl dark:shadow-gray-800 relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
     <a href="/list">
       <img class="rounded-lg" src="images/spider-man-homecoming.jpg" alt="image description">
     </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Phase 3</p>
-    </figcaption>
   </figure>
+  <p class="font-[WorkSans] text-white">The story that started them off</p>
   <h1 class="font-[WorkSans] text-3xl text-white">Phase 3</h1>
-  <p class="font-[WorkSans] text-white">Tipping the cosmic scales to balance</p>
   
   <figure class="h-auto max-w-md rounded-lg shadow-xl dark:shadow-gray-800 relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
     <a href="/list">
       <img class="rounded-lg" src="images/spider-no-way-home.jpg" alt="image description">
     </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Phase 4</p>
-    </figcaption>
   </figure>
+  <p class="font-[WorkSans] text-white">To New Beginnings</p>
   <h1 class="font-[WorkSans] text-3xl text-white">Phase 4</h1>
-  <p class="font-[WorkSans] text-white">The world sure is different (After the Blip)</p>
   
   <figure class="h-auto max-w-md rounded-lg shadow-xl dark:shadow-gray-800 relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
     <a href="list">
       <img class="rounded-lg" src="images/the-marvels.jpg" alt="image description">
     </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Phase 5</p>
-    </figcaption>
   </figure>
+  <p class="font-[WorkSans] text-white">The world sure is different (After the Blip)</p>
   <h1 class="font-[WorkSans] text-3xl text-white">Phase 5</h1>
-  <p class="font-[WorkSans] text-white">To New Beginnings</p>
   </div>
 </body>
   
-
-<!-- <body class="bg-orange-950">
-  
-
-<div class="grid grid-cols-2 md:grid-cols-2 gap-4">
-  <div>
-      <img class="h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="images/avengers-1.jpg" alt="">
-  </div>
-  <div>
-      <img class="h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="images/guardians-of-the-galaxy.jpg" alt="">
-  </div>
-  <div>
-      <img class="h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="images/spider-man-homecoming.jpg" alt="">
-  </div>
-  <div>
-      <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
-  </div>
-  <div>
-      <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
-  </div>
-  <div>
-      <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
-  </div>
-  
- 
-
-</body> -->
