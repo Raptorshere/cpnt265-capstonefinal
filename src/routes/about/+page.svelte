c2537b33184040ca6cddb29c6ce3dc731ae6ed41
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-11 12:48:22 -0700

    typo mistake

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,10 @@
 <body class="bg-orange-950 text-3xl text-white font- [WorkSans]">
 
 <img class="rounded-lg w-98 h-76" src="images/allphases-collection.jpg" alt="grid-style image of marvel related movies that span from each phase">
-<img class="rounded-lg w-98 h-76" src="images/marvel-tvshows.jpg" alt="">
+<img class="rounded-lg w-98 h-76" src="images/marvel-tvshows.jpg" alt="grid-style image of marvel related tv shows from phase 4">
 <div>
-  <span>
+  
+    <span class="md: flex">
     Welcome to a snippet of my Marvel collection which features all Marvel based movies that spanned from 2008 to present-day. 
   All my years of Marvel movie watching has added up to my inspiration to create this grouped out assorted cards that feature how each Marvel movie are sized down into specific 'phases' that tie in to the Marvel Cinematic Universe.
   Hope you enjoy!
