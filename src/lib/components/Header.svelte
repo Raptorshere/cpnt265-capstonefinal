a14c04ce9c809f700e5ddde8b9321b714e45a418
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-11 15:30:16 -0700

    paddign change in About page bottom text

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,8 @@
 <!-- Tailwind Flowbite-->
 
 
-<header class="">
+<header class="md:flex">
+  <div class="md:shrink-0">
 <nav class="bg-orange-950">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -30,6 +31,7 @@
     </div>
   </div>
 </nav>
+  </div>
 </header>
 
 
